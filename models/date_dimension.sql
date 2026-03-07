7abbf2f536c57a997bef1ad8aec6d03d9afd786f
anouar.bouk@gmail.com <anouar.bouk@gmail.com>
2026-03-07 01:59:10 +0000

    daily_weather model created

--- models/date_dimension.sql
+++ models/date_dimension.sql
@@ -4,21 +4,9 @@ with cte as (
     date(to_timestamp(started_at)) as date_started_at,
     hour(to_timestamp(started_at)) as hour_started_at,
 
-    case 
-        when dayname(to_timestamp(started_at)) in ('Sat', 'Sun')
-            then 'WEEKEND'
-        else 'BUSINESSDAY'
-    end as day_type,
+    {{day_type('started_at')}} as day_type,
 
-   case when month(to_timestamp(started_at)) in (12, 1, 2)
-        then 'winter'
-        when month(to_timestamp(started_at)) in (3, 4, 5)
-            then 'SPRING'
-        when month(to_timestamp(started_at)) in (6, 7, 8)
-            then 'SUMMER'
-        else 'AUTUMN'
-    end as station_of_year
-
+    {{get_season('started_at')}} as station_of_year
 
 
      
